67fff1b5759e9a0c8681445b2164a048527d7e25
Jason Louv <jason@magick.me>
2022-07-22 20:48:43 -0500

    Initial commit

---
+++ Zookeepr/Zookeepr/ZookeeprApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZookeeprApp.swift
+//  Zookeepr
+//
+//  Created by Order on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ZookeeprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
