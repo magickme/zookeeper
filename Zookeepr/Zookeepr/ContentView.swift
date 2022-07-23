80dfb64da1a7d6e277f7ee69a60666c17778d9c1
Jason Louv <jason@magick.me>
2022-07-22 21:13:29 -0500

    Part 1 done

--- Zookeepr/Zookeepr/ContentView.swift
+++ Zookeepr/Zookeepr/ContentView.swift
@@ -1,18 +1,61 @@
 //
 //  ContentView.swift
-//  Zookeepr
+//  Shared
 //
-//  Created by Order on 7/22/22.
+//  Created by Henry Minden on 7/28/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(animalGroups) { animalGroup in
+                Section(header: Text(animalGroup.groupName)) {
+                    ForEach(animalGroup.animals) { animal in
+                        Text(animal.name)
+                    }
+                }
+            }
+        }
     }
+
 }
+
+struct Animal: Identifiable {
+    var id = UUID()
+    var name : String
+}
+
+struct AnimalGroup: Identifiable {
+    var id = UUID()
+    var groupName : String
+    var animals : [Animal]
+}
+
+let animalGroups = [
+    AnimalGroup(groupName: "Pets", animals: [
+        Animal(name: "Dog"),
+        Animal(name: "Cat"),
+        Animal(name: "Parrot"),
+        Animal(name: "Mouse")
+    ]),
+    AnimalGroup(groupName: "Farm", animals: [
+        Animal(name: "Cow"),
+        Animal(name: "Horse"),
+        Animal(name: "Goat"),
+        Animal(name: "Sheep"),
+    ]),
+    AnimalGroup(groupName: "Critters", animals: [
+        Animal(name: "Firefly"),
+        Animal(name: "Spider"),
+        Animal(name: "Ant"),
+        Animal(name: "Squirrel"),
+    ])
+]
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
